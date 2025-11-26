f894022deb2526a93c5a5e44c100a6c7ea6db64e
PrithiviPM2580 <prithivipemimagar>
2025-11-27 02:11:48 +0545

    Add Express application setup with middleware and server initialization

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+// ============================================================
+// 🧩 APP — Express application setup
+// ============================================================
+
+import cookieParser from "cookie-parser";
+import express, { type Express } from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import logger from "@/lib/logger.lib";
+
+// Initialize the Express application
+const app: Express = express();
+
+import globalErrorHandlerMiddleware from "@/middleware/global-error-handler.middleware";
+// ------------------------------------------------------
+// Imports
+// ------------------------------------------------------
+import routes from "@/routes/index.route";
+
+// ------------------------------------------------------
+// Middlewares
+// ------------------------------------------------------
+app.use(helmet());
+app.use(
+	morgan("combined", {
+		stream: { write: (message) => logger.info(message.trim()) },
+	}),
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(routes);
+app.use(globalErrorHandlerMiddleware);
+
+export default app;
