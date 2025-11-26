f2c138e4f4b7c051924a785aeff4cc139e073677
PrithiviPM2580 <prithivipemimagar>
2025-11-27 02:11:53 +0545

    Add environment configuration with validation

---
+++ src/config/env.config.ts
@@ -0,0 +1,31 @@
+// ============================================================
+// 🧩 EnvConfig — Environment configuration
+// ============================================================
+import "dotenv/config";
+import validate from "@/lib/validate.lib";
+import envSchema from "@/validator/env.validator";
+
+// ------------------------------------------------------
+// envConfig{} — Environment configuration object
+// ------------------------------------------------------
+const envConfig = {
+	NODE_ENV: process.env.NODE_ENV,
+	PORT: process.env.PORT,
+	APP_VERSION: process.env.APP_VERSION,
+	DB_HOST: process.env.DB_HOST,
+	DB_PORT: process.env.DB_PORT,
+	DB_USER: process.env.DB_USER,
+	DB_PASSWORD: process.env.DB_PASSWORD,
+	DB_NAME: process.env.DB_NAME,
+	DATABASE_URL: process.env.DATABASE_URL,
+	LOG_LEVEL: process.env.LOG_LEVEL,
+	JWT_ACCESS_TOKEN_SECRET: process.env.JWT_ACCESS_TOKEN_SECRET,
+	JWT_ACCESS_TOKEN_EXPIRATION: process.env.JWT_ACCESS_TOKEN_EXPIRATION,
+	JWT_REFRESH_TOKEN_SECRET: process.env.JWT_REFRESH_TOKEN_SECRET,
+	JWT_REFRESH_TOKEN_EXPIRATION: process.env.JWT_REFRESH_TOKEN_EXPIRATION,
+};
+
+// Configure and validate the environment variables
+const config = validate(envSchema, envConfig);
+
+export default config;
